2c2b8c0b3d8b8084fecc5ecb25e83359ea1c1f05
Dylan Turner <dylantdmt@gmail.com>
2020-12-17 13:28:57 -0600

    Added ebnf for assembler

--- assembler/src/Program.cs
+++ assembler/src/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace assembler
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace assembler {
+    class Program {
+        static void Main(string[] args) {
+            
         }
     }
 }
