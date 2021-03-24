2ef2a880a5ee3775cd6c34889faeba89c0f5537e
Dylan Turner <dylantdmt@gmail.com>
2021-03-24 19:22:45 -0400

    Implemented multicore example test

--- pico/src/main.cpp
+++ pico/src/main.cpp
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
+#include <pico/multicore.h>
 #include <Oled.hpp>
 #include <Controls.hpp>
 
 using namespace gamecard;
 
+void testButtons();
 void testHardware();
+void testHardwareMulticore();
+void testHardwareMulticoreCore1();
 
 int main() {
     stdio_init_all();
     printf("Welcome to gamecard!\n");
-    testHardware();
+    
+    //testHardware();
+    testHardwareMulticore();
+    
     return 0;
+}
+
+void testHardwareMulticore() {
+    multicore_launch_core1(testHardwareMulticoreCore1);
+    const Ssd1306 oled;
+    while(1) {
+        oled.test();
+    }
+}
+
+void testHardwareMulticoreCore1() {
+    testButtons();
 }
 
 void testHardware() {
     const Ssd1306 oled;
+    oled.test();
+    
+    testButtons();
+}
+
+void testButtons() {
     const Controller cont;
     
-    oled.test();
     while(1) {
         printf("Reading controller: ");
         if(cont.isPressed(ControllerButton::Up)) {
