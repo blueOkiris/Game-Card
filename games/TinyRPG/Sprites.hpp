989a52dd48288c63761b789ff07a3b9eb30879ee
Dylan Turner <dylantdmt@gmail.com>
2021-08-17 00:10:39 -0500

    Started on tiny rpg that can fit into the 8kB

---
+++ games/TinyRPG/Sprites.hpp
@@ -0,0 +1,10 @@
+/*
+ * Author: Dylan Turner
+ * Description: Sprites definition for Tiny RPG. 
+ */
+
+#ifndef _SPRITES_HPP_
+#define _SPRITES_HPP_
+
+
+#endif
---
+++ games/TinyRPG/TinyRPG.ino
@@ -0,0 +1,20 @@
+/*
+ * Author: Dylan Turner
+ * Description: Main entry port for tiny RPG
+ */
+
+#include <GameCardDisplay.hpp>
+#include <GameCardControls.hpp>
+#include "State.hpp"
+#include "Sprites.hpp"
+
+const gamecard::Ssd1306 g_disp;
+const gamecard::ButtonController g_cont;
+
+void setup(void) {
+
+}
+
+void loop(void) {
+
+}
